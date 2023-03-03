2af8edc19f0a9e38b2df9e3e160d82e5a08cf292
Geraskin Denis <fresh7tea@gmail.com>
2023-03-03 23:05:08 +0300

    Added Stars.java

---
+++ src/geraskindenis/hw1/stars/Stars.java
@@ -0,0 +1,22 @@
+package geraskindenis.hw1.stars;
+
+import java.util.Scanner;
+
+public class Stars {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int lines = scanner.nextInt();
+            int columns = scanner.nextInt();
+            String template = scanner.next();
+
+            if (columns == 0) {
+                return;
+            }
+
+            for (int line = 0; line < lines; line++) {
+                System.out.println((template + " ").repeat(columns - 1) + template);
+            }
+
+        }
+    }
+}
