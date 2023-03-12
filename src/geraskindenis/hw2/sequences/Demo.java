300e1afe9daee2f72f136ea552120a11c83e8f0d
Geraskin Denis <fresh7tea@gmail.com>
2023-03-12 22:50:29 +0300

    HW2: Add Sequences

---
+++ src/geraskindenis/hw2/sequences/Demo.java
@@ -0,0 +1,37 @@
+package geraskindenis.hw2.sequences;
+
+public class Demo {
+    public static void main(String[] args) {
+        SequenceGeneratorImpl sequenceGeneratorImpl = new SequenceGeneratorImpl();
+
+        System.out.println("---Sequences A---");
+        sequenceGeneratorImpl.a(5);
+
+        System.out.println("---Sequences B---");
+        sequenceGeneratorImpl.b(5);
+
+        System.out.println("---Sequences C---");
+        sequenceGeneratorImpl.c(5);
+
+        System.out.println("---Sequences D---");
+        sequenceGeneratorImpl.d(5);
+
+        System.out.println("---Sequences E---");
+        sequenceGeneratorImpl.e(6);
+
+        System.out.println("---Sequences F---");
+        sequenceGeneratorImpl.f(6);
+
+        System.out.println("---Sequences G---");
+        sequenceGeneratorImpl.g(5);
+
+        System.out.println("---Sequences H---");
+        sequenceGeneratorImpl.h(7);
+
+        System.out.println("---Sequences I---");
+        sequenceGeneratorImpl.i(6);
+
+        System.out.println("---Sequences J---");
+        sequenceGeneratorImpl.j(8);
+    }
+}
