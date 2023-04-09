a0793e47ab62781b893a37e93544670d15573fea
Geraskin Denis <fresh7tea@gmail.com>
2023-04-09 22:54:46 +0300

    Lesson #5: HW #5 Changed query

--- src/main/java/io/ylab/intensive/lesson05/messagefilter/message/MessageHandlerImpl.java
+++ src/main/java/io/ylab/intensive/lesson05/messagefilter/message/MessageHandlerImpl.java
@@ -1,10 +1,7 @@
 package io.ylab.intensive.lesson05.messagefilter.message;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,12 +18,12 @@ public class MessageHandlerImpl extends MessageHandlerAbs {
     @Override
     protected List<String> getSimilarStrings(String[] subStrings) {
         List<String> similarStrings = new ArrayList<>();
-        String pattern = "SELECT badword FROM badwords WHERE badword IN (%s)";
-        String str = arrayToString(subStrings);
-        String query = String.format(pattern, str);
+        String query = "SELECT badword FROM badwords WHERE badword = ANY(?)";
         try (Connection connection = dataSource.getConnection();
-             Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery(query);
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            Array array = connection.createArrayOf("VARCHAR", subStrings);
+            statement.setArray(1, array);
+            ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
                 similarStrings.add(resultSet.getString(1));
             }
@@ -39,17 +36,5 @@ public class MessageHandlerImpl extends MessageHandlerAbs {
     @Override
     protected void sendMessage(String correctMessage) {
         messageSender.sendMessage(correctMessage);
-    }
-
-    private String arrayToString(String[] strings) {
-        String pattern = "'%s'";
-        StringBuilder builder = new StringBuilder();
-        for (String str : strings) {
-            if (builder.length() > 0) {
-                builder.append(", ");
-            }
-            builder.append(String.format(pattern, str));
-        }
-        return builder.toString();
     }
 }
